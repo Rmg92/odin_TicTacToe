1c00f6d9267af44e5a34033b50a939f9377f13ae
Ricardo <ricardomg92@hotmail.com>
2022-01-22 07:58:27 +0100

    Added a board class that will create the board

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -4,7 +4,7 @@ require 'pry-byebug'
 
 # Stores winning conditions
 module Win
-  win = [1, 2, 3]
+  WINNINGCONDITIONS = [1, 2, 3].freeze
 end
 
 # Creates a new player
@@ -16,12 +16,27 @@ class Player
   end
 end
 
+# Contains all the game logic
 class Game
 end
 
-puts 'Enter player 1 name'
-player1 = Player.new(gets.chomp)
-puts 'Enter player 2 name'
-player2 = Player.new(gets.chomp)
+# contains the elements to create a new board
+class Board
+  def board
+    puts '1 | 2 | 3'
+    puts '- + - + -'
+    puts '4 | 5 | 6'
+    puts '- + - + -'
+    puts '7 | 8 | 9'
+  end
+end
 
-puts player1.name, player2.name
+# puts 'Enter player 1 name'
+# player1 = Player.new(gets.chomp)
+# puts 'Enter player 2 name'
+# player2 = Player.new(gets.chomp)
+
+# puts player1.name, player2.name
+
+board = Board.new
+puts board.board
