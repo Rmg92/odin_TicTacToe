5df5bc30b870bd5b1c262a86c05be3b185fb88c4
Ricardo <ricardomg92@hotmail.com>
2022-01-23 07:46:01 +0100

    Play method now plays 5 rounds with 1 player

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -18,28 +18,36 @@ end
 
 # Contains all the game logic
 class Game
+  @@board_positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
   def initialize(player_class)
     puts "What's your name player 1?"
     player1_name = gets.chomp
     puts "What's your name player 2?"
     player2_name = gets.chomp
     @players = [player_class.new(player1_name), player_class.new(player2_name)]
+    print_board(0, 1)
     play
   end
 
   def print_board(position, marker)
-    board_positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    board_positions[position] = marker
-    puts "#{board_positions[0]} | #{board_positions[1]} | #{board_positions[2]}"
+    @@board_positions[position] = marker
+    puts "#{@@board_positions[0]} | #{@@board_positions[1]} | #{@@board_positions[2]}"
     puts '- + - + -'
-    puts "#{board_positions[3]} | #{board_positions[4]} | #{board_positions[5]}"
+    puts "#{@@board_positions[3]} | #{@@board_positions[4]} | #{@@board_positions[5]}"
     puts '- + - + -'
-    puts "#{board_positions[6]} | #{board_positions[7]} | #{board_positions[8]}"
+    puts "#{@@board_positions[6]} | #{@@board_positions[7]} | #{@@board_positions[8]}"
   end
 
   def play
-    puts @players[1].name
-    print_board(0, 'X')
+    round_number = 0
+    until round_number == 9
+      puts "It's your turn #{@players[0].name}, choose a position!"
+      position = gets.to_i
+      puts "#{@players[0].name} chose position #{position}"
+      print_board(position - 1, 'X')
+      round_number += 1
+    end
   end
 end
 
